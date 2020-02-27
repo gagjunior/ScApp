af8ec38480b6782196281194af6481e69509984e
gagjunior <48300739+gagjunior@users.noreply.github.com>
2020-02-27 15:24:16 -0300

    Atualizar ordenação lista
    Atualizar ordenação da lista de publicadores

--- publicadores/static/javascript/publicadores.js
+++ publicadores/static/javascript/publicadores.js
@@ -7,3 +7,18 @@ let dataFormatada = `${dia}/${mes}/${ano}`
 
 document.getElementById('data-atual').innerText = dataFormatada
 
+var currentImgIndex=1
+var igmSrcArray = [
+    "/static/imagens/seta_cima.png",
+    "/static/imagens/seta_baixo.png"    
+]
+
+function alteraImagem(){
+    if(currentImgIndex == igmSrcArray.length){
+        currentImgIndex=0
+    }
+    document.getElementById("order").src=igmSrcArray[currentImgIndex]
+    currentImgIndex++
+    
+}
+
