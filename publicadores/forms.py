ced6dd00bb002c8728a1707c5236a594e488a005
gagjunior <48300739+gagjunior@users.noreply.github.com>
2020-03-12 14:59:13 -0300

    Atualização Fomulario Pesquisa

--- publicadores/forms.py
+++ publicadores/forms.py
@@ -1,10 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from publicadores.models import Atividade
 from publicadores import data_atual, LISTA_MES
 
+mes_anterior = data_atual.month - 1
+ano_atual = data_atual.year
 
-class AtividadeForm(ModelForm):
-
-    class Meta:
-        model = Atividade
-        fields = ['mes_relatorio', 'ano_relatorio']
+class AtividadeForm(forms.Form):
+    mes_inicio = forms.ChoiceField(choices=LISTA_MES, initial=mes_anterior)
+    ano_inicio = forms.IntegerField(initial=ano_atual)
+    mes_fim = forms.ChoiceField(choices=LISTA_MES, initial=mes_anterior)
+    ano_fim = forms.IntegerField(initial=mes_anterior)
